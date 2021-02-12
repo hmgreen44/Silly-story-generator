3e60317f3bb23e9ede2f7f769873a5bb349d17a7
hmgreen44 <68399145+hmgreen44@users.noreply.github.com>
2021-02-12 12:29:39 -0500

    dev

--- main.js
+++ main.js
@@ -7,40 +7,40 @@ function randomValueFromArray(array){
   return array[random];
 }// Added variables to fill in the story from a random array //
 
-var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.";
-var insertX = "Willy the Goblin, Big Daddy, Father Christmas";
-var insertY = "the soup kitchen, Disneyland, the White House";
-var insertZ = "spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away";
+var storyText = "It was 94 fahrenheit outside, so :insertX: went for a walk. When they got to :insertY:, they stared in horror for a few moments, then :insertZ:. Bob saw the whole thing, but was not surprised — :insertX: weighs 300 pounds, and it was a hot day.";
+let insertX = ['Willy the Goblin','Big Daddy','Father Christmas'];
+let insertY = ['the soup kitchen','Disneyland','the White House'];
+let insertZ = ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
 //adding a replacement of the newStory with an X Y and Z item//
 
-  randomize.addEventListener('click', result);
+randomize.addEventListener('click', result);
 
-  function result() {    
+function result() {    
     //Allows a newStory to be created//
-    var newStory = storyText;
+let newStory = storyText;
     //using let to grab a random value from the X array//
-  var xItem = randomValueFromArray(insertX)
-  var yItem = randomValueFromArray(insertY)
-  var zItem = randomValueFromArray(insertZ)
+  let xItem = randomValueFromArray(insertX)
+  let yItem = randomValueFromArray(insertY)
+  let zItem = randomValueFromArray(insertZ)
 
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':insertx:',xItem);
-  newStory = newStory.replace(':inserty:',yItem);
-  newStory = newStory.replace(':insertz:',zItem);
+  newStory = newStory.replace(':insertX:',xItem);
+  newStory = newStory.replace(':insertX:',xItem);
+  newStory = newStory.replace(':insertY:',yItem);
+  newStory = newStory.replace(':insertZ:',zItem);
 
 
   if(customName.value !== '') {
-    //const used to assign a value and have a constant reference to that value//
+    //if name is not equal to no input then input customer name. If nothing is submitted replace name to Bob//
     const name = customName.value;
-    newStory = newStory.replace('Bob',customName);
+    newStory = newStory.replace('Bob',name);
 
   }
 
   if(document.getElementById("uk").checked) {
     //using const for another variable with a stones, and centigrade//
     const weight = Math.round(300*0.071429) + 'stone';
-    let temperature =  Math.round((94-32) * 5/9); + 'centigrade';
-    //if Uk is checked replace values of weight and temperature with UK 
+    const temperature =  Math.round((94-32) * 5/9); + 'centigrade';
+    //if Uk box is checked replace values of weight and temperature with UK measurements 
     newStory = newStory.replace ('300 pounds', weight)
     newStory = netStory. replace ('94 fahrenheit', temperature)
   }
